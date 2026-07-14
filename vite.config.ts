285e286f42ca1f18564e92906a46a5a6a6e104ba
[dyad] <albymnzmanfady@gmail.com>
2026-07-14 15:48:31 +0300

    [dyad] إزالة التظليل من النظام بحذف مكون التتبع dyadComponentTagger من إعدادات Vite - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
@@ -14,7 +13,7 @@ export default defineConfig(() => ({
       },
     },
   },
-  plugins: [dyadComponentTagger(), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
